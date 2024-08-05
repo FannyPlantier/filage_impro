48d44c0b709c03b95ed1ba6b55a50d35d595400f
Fanny <fannyrouxfr@gmail.com>
2024-08-05 16:47:27 +0200

    fix style problem

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'main_')]
 class MainController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
+        return $this->render('main/index.html.twig',
+        [
         ]);
     }
 }
